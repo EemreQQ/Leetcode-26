736f607382889fa9c81098a4b810e1b5a6fd2ddf
emre erol <emre_erol17@hotmail.com>
2023-09-17 16:57:02 +0300

    Proje dosyası ekle.

---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Linq;
+public class Solution
+{
+    public int RemoveDuplicates(params int[] nums)
+    {
+            List<int> values = new List<int>();
+            Hashtable hashtable = new Hashtable();
+            for (int i = 0; i < nums.Length; i++)
+             {
+            if (hashtable.Contains(nums[i]) == false)
+            {
+                hashtable.Add(nums[i], i);
+                values.Add(nums[i]);
+            }
+             }
+        Array.Copy(values.ToArray(),0, nums, 0,values.Count);
+        return values.Count;
+    }
+}
